a04d05ce6ccb4b0a617d3994d6c1f88acc78a2e9
macro6461 <mattcroak718@gmail.com>
2022-04-22 14:48:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Changelog JavaScript
+
+This is the pure javascript code for creating and updating a changelog.
+
+## Benefits 
+- No need for generators.
+- No need for dependencies and other third parties.
+- No unnecessary/overwhelming log dump.
+- More flexibility for layout and formats.
+
+## Usage
+- Create a `changelog.js` python file.
+- Copy the code from this project's `changelog.js` file.
+- Run `chmod +x changelog.js` to make it executable.
+- Run `./changelog.js` and sit back while a Changelog is created or new changelog item is added that is aligned with today's date and version.
+
+## Supported Languages (with repo links)
+- [Bash](https://github.com/macro6461/changelog-bash)
+- JavaScript (TBD)
+- [Python](https://github.com/macro6461/changelog-python)
---
+++ changelog.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+const {readFileSync, writeFileSync, appendFile, existsSync} = require('fs');
+const path = './CHANGELOG.md'
+// if you download a package to help run git log --describe
+// you can remove the below require statement, the result function
+// and in getTaggedVersion put package logic
+const exec = require('child_process').exec;
+
+const result = function(command, cb){
+    var child = exec(command, function(err, stdout, stderr){
+        if(err != null){
+            return cb(new Error(err), null);
+        }else if(typeof(stderr) != "string"){
+            return cb(new Error(stderr), null);
+        }else{
+            return cb(null, stdout);
+        }
+    });
+    return child
+}
+
+const getTaggedVersion = () => {
+    result("git describe --long", function(err, response){
+        if(!err){
+            console.log(response);
+        }else {
+            console.log(err);
+        }
+    });
+}
+
+const d = new Date()
+const today=d.toISOString().slice(0, 10)
+var version="1.2.3"
+// uncomment below line to get tagged version with git describe --long
+// var version = getTaggedVersion()
+
+const item = `## [${version}] - ${today}`
+
+const changelogStart=`# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+${item}
+### Added
+- ADD CHANGE HERE!
+`
+
+const checkIfItemExists = (str) => {
+    const contents = readFileSync('CHANGELOG.md', 'utf-8');
+
+    const result = contents.includes(str);
+
+    return result;
+}
+
+const writeChangelog = () =>{
+    const fileData = readFileSync("CHANGELOG.md", { encoding: "utf8" });
+    const fileDataArray = fileData.split("\n");
+    const iterateArr = [...fileDataArray]
+    for (var i = 0; i < iterateArr.length; i++){
+        if (fileDataArray[i].indexOf("## [Unreleased]") > -1){
+            fileDataArray.splice(i + 1, 0, `\n${item}\n### Added\n- ADD CHANGE HERE!`);
+            const newFileData = fileDataArray.join("\n");
+            writeFileSync("CHANGELOG.md", newFileData, { encoding: "utf8" });
+            break
+        }
+    }
+}
+
+const newChangelog = () => {
+    appendFile("CHANGELOG.md", changelogStart, (err) => {
+        if (err) throw err;
+        console.log('Changelog is created successfully.');
+      })
+}
+
+const newChangelogItem = () => {
+    if (checkIfItemExists(item)){
+        console.log(`Changelog item already exists for \n   ${item}`)
+    } else {
+        writeChangelog()
+    }
+}
+
+const init = () => {
+    try {
+        if (existsSync(path)) {
+            newChangelogItem()
+        } else {
+            newChangelog()
+        }
+      } catch(err) {
+        console.error(err)
+      }
+}
+
+init()
